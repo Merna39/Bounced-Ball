b421e36cd4b5e786ca86409ce1b9f9961be86472
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-26 22:23:18 +0200

    Update Main.java

--- Final_project/Main.java
+++ Final_project/Main.java
@@ -1,15 +1,12 @@
-
 package Final_project;
 
-
-
-
-
 public class Main {
+    
     public static void main(String[] args) {
+        
         Gameplay gamePlay = new Gameplay();
        new HomePage(gamePlay);
         
-
     }
+    
 }
