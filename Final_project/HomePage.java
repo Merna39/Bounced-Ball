6c3e03c42a56f3de0a4036e06c88a8cde8b89a3d
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-26 22:35:42 +0200

    Update HomePage.java

--- Final_project/HomePage.java
+++ Final_project/HomePage.java
@@ -19,6 +19,22 @@ public class HomePage extends JFrame{
        frame.setLayout(new FlowLayout(600, 300, 150));
        JButton button1 = new JButton("          play      ");
        button1.setBackground(Color.yellow);
+       button1.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent ae) {
+         
+            JFrame theFrame = new JFrame("Bounced Ball");
+            theFrame.setBounds(1,1,700,600);
+            theFrame.setResizable(false);
+            theFrame.setLocation(500, 300);
+            theFrame.add(gamePlay);
+        
+            theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            theFrame.setVisible(true);
+          }
+       });
+       
+       frame.add(button1);
        
   }
 }
