c70794ec06ccfd04559b3b1375834c29113d1f7d
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-26 22:19:34 +0200

    Update Main.java

--- Final_project/Main.java
+++ Final_project/Main.java
@@ -8,10 +8,7 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         Gameplay gamePlay = new Gameplay();
-       JFrame theFrame = new JFrame("Bounced Ball");
-       theFrame.setBounds(10,10,1000,700);
-        theFrame.setResizable(false);
-        theFrame.setLocation(300, 300);
+       
         
 
     }
