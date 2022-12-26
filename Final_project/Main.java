df2442cacc1b37460db20f90c172fdb959ae174d
Merna Ashraf <116306724+Merna39@users.noreply.github.com>
2022-12-26 05:18:59 -0800

    Update Main.java

--- Final_project/Main.java
+++ Final_project/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Final_project;
 
 import java.awt.Color;
@@ -13,13 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Gameplay gamePlay = new Gameplay();
        JFrame theFrame = new JFrame("Bounced Ball");
-       theFrame.setBounds(10,10,700,600);
+       theFrame.setBounds(10,10,1000,700);
         theFrame.setResizable(false);
-//       theFrame.setLocationRelativeTo(null);
         theFrame.setLocation(300, 300);
         theFrame.add(gamePlay);
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.setVisible(true);
-//        thePanel.playGame();
+
     }
 }
